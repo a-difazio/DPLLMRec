31bed403e599afc722b6587aa153daa48d565cc2
a-difazio <angela_933@hotmail.it>
2026-03-19 17:56:40 +0000

    changed training configuration music and games

--- SASRec/run_train_games.sh
+++ SASRec/run_train_games.sh
@@ -4,14 +4,15 @@ echo "=== SASRec CE Full Vocab Pretraining (Games) ==="
 
 python train.py \
     --dataset amazon_games \
-    --run_name v3 \
-    --maxlen 50 \
+    --run_name maxlen30_v4 \
+    --maxlen 30 \
     --hidden_units 50 \
     --num_heads 1 \
-    --num_blocks 2 \
+    --num_blocks 1 \
     --dropout_rate 0.5 \
-    --batch_size 128 \
-    --lr 0.001 \
+    --batch_size 256 \
+    --lr 0.0001 \
+    --l2_emb 1e-4 \
     --num_epochs 1000 \
     --patience 10 \
     --eval_interval 5 \
--- SASRec/run_train_music.sh
+++ SASRec/run_train_music.sh
@@ -4,14 +4,15 @@ echo "=== SASRec CE Full Vocab Pretraining (Music) ==="
 
 python train.py \
     --dataset amazon_music \
-    --run_name v3 \
-    --maxlen 50 \
+    --run_name maxlen20_v4 \
+    --maxlen 20 \
     --hidden_units 50 \
     --num_heads 1 \
-    --num_blocks 2 \
+    --num_blocks 1 \
     --dropout_rate 0.5 \
-    --batch_size 128 \
-    --lr 0.001 \
+    --batch_size 256 \
+    --lr 0.0001 \
+    --l2_emb 1e-4 \
     --num_epochs 1000 \
     --patience 10 \
     --eval_interval 5 \
