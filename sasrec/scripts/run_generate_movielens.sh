fb763cde0870ff7efbfad328cde2a4c1c76650c9
a-difazio <angela_933@hotmail.it>
2026-03-24 10:50:43 +0100

    updated device option

--- sasrec/scripts/run_generate_movielens.sh
+++ sasrec/scripts/run_generate_movielens.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # run_generate_movielens.sh
 
-echo '=== Generating for movielens ==='
+DEVICE=${1:-cuda}  # default cuda, override con: bash script.sh mps
+
+echo '=== Generating for movielens (device: $DEVICE) ==='
 
 echo 'Running temp0.5_toppnone_topknone_pen0.0'
 python generate.py \
@@ -9,7 +11,7 @@ python generate.py \
     --run_name final \
     --gen_name temp0.5_toppnone_topknone_pen0.0 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 0.5 \
     --penalty 0.0 \
     --seed 42
@@ -20,7 +22,7 @@ python generate.py \
     --run_name final \
     --gen_name temp0.7_toppnone_topknone_pen0.0 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 0.7 \
     --penalty 0.0 \
     --seed 42
@@ -31,7 +33,7 @@ python generate.py \
     --run_name final \
     --gen_name temp1.0_toppnone_topknone_pen0.0 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 1.0 \
     --penalty 0.0 \
     --seed 42
@@ -42,7 +44,7 @@ python generate.py \
     --run_name final \
     --gen_name temp1.2_toppnone_topknone_pen0.0 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 1.2 \
     --penalty 0.0 \
     --seed 42
@@ -53,7 +55,7 @@ python generate.py \
     --run_name final \
     --gen_name temp1.0_topp0.9_topknone_pen0.0 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 1.0 \
     --penalty 0.0 \
     --seed 42 \
@@ -65,7 +67,7 @@ python generate.py \
     --run_name final \
     --gen_name temp1.0_topp0.95_topknone_pen0.0 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 1.0 \
     --penalty 0.0 \
     --seed 42 \
@@ -77,7 +79,7 @@ python generate.py \
     --run_name final \
     --gen_name temp1.0_toppnone_topk10_pen0.0 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 1.0 \
     --penalty 0.0 \
     --seed 42 \
@@ -89,7 +91,7 @@ python generate.py \
     --run_name final \
     --gen_name temp1.0_toppnone_topk50_pen0.0 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 1.0 \
     --penalty 0.0 \
     --seed 42 \
@@ -101,7 +103,7 @@ python generate.py \
     --run_name final \
     --gen_name temp1.0_toppnone_topknone_pen0.1 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 1.0 \
     --penalty 0.1 \
     --seed 42
@@ -112,7 +114,7 @@ python generate.py \
     --run_name final \
     --gen_name temp1.0_toppnone_topknone_pen0.3 \
     --checkpoint checkpoint_best.pth \
-    --device cuda \
+    --device $DEVICE \
     --temperature 1.0 \
     --penalty 0.3 \
     --seed 42
